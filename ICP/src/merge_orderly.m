e909aef80b065bfb322900b78b7a992a0781b5a9
Tommy Wang <tjbwyk@gmail.com>
2014-04-24 02:15:02 +0200

    ICP checked to be right, merge algorithm 1 implemented

--- ICP/src/merge_orderly.m
+++ ICP/src/merge_orderly.m
@@ -1,3 +1,26 @@
-function [points_merged] = merge_orderly(inter)
-
+function [points_merged] = merge_orderly(interval)
+    
+    for i = 0 : interval : 99
+        i
+        filename = sprintf('../data/%010d_filtered.txt', i);
+        source = load(filename);
+        if i == 0
+            Tr_all = eye(4);
+            target = source;
+            points_merged = source;
+        else
+            [R, t, Tr, source_homo, target_homo] = ICP(source, target, 0.01, 20);
+            Tr_all = Tr * Tr_all;
+            source_homo_trans = Tr_all * source_homo;
+            
+            KDTree = KDTreeSearcher(points_merged);
+            [IDX, D] = knnsearch(KDTree, source_homo_trans(1:3, :)');
+            points_merged(IDX, :) = [];
+            points_merged = [points_merged; source_homo_trans(1:3, :)'];
+            
+            target = source;
+        end
+        plot3(points_merged(:, 1), points_merged(:, 2), points_merged(:, 3), '.');
+        pause(0.1);
+    end
 end

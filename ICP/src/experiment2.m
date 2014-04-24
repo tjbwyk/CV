34fe1f9f1bb1cfa68078505cf9574c0451bbd432
Arif Qodari <arif.qodari@gmail.com>
2014-04-24 11:31:53 +0200

    Finish code for experiment 2.2

--- ICP/src/experiment2.m
+++ ICP/src/experiment2.m
@@ -1,24 +1,57 @@
 function experiment2()
+  % Define minimum treshold and number of maximum iterations
   TOL = 0.0001;
   TIME = 20;
 
+  % Define data directory
   files = dir('../data/*.txt');
-  file1 = files(1);
 
-  S = importdata(strcat('../data/', file1.name));
-  len = 5;
+  % Number of files
+  len = 20;
+
+  % Initialize Source Points
+  file1 = files(1);
+  Source = importdata(strcat('../data/', file1.name));
+
+  % Initialize figure
+  figure;
+  hold on;
+  h1 = plot3(Source(:, 1), Source(:, 2), Source(:, 3), 'bo');
+  h2 = plot3(Source(:, 1), Source(:, 2), Source(:, 3), 'ro');
 
   for ii=2:len
+    ii
+
+    % Define Target points
     file2 = files(ii);
-    T = importdata(strcat('../data/', file2.name));
+    Target = importdata(strcat('../data/', file2.name));
 
-    [R, t] = ICP(S, T, TOL, TIME);
+    % Apply ICP
+    [R, t, Tr, Source_h, Target_h] = ICP(Source, Target, TOL, TIME);
 
-    S_new = S * R + repmat(t',length(S),1);
-    S_new = [S_new;T];
-    S = S_new;
+    % Transform Source Points
+    Source_h_trans = Tr * Source_h;
 
-    size(S)
+    % Find matched point between Source and Target
+    KDTree = KDTreeSearcher(Target, 'distance', 'euclidean');
+    [IDX, D] = knnsearch(KDTree, Source_h_trans(1:3,:)');
+
+    % Remove matched points from Target
+    Target(unique(IDX), :) = [];
+
+    % Merge points Source and Target
+    Source_new = [Source_h_trans(1:3,:)';Target];
+
+    % Plot result
+    delete(h1);
+    delete(h2);
+    h1 = plot3(Source_h_trans(1, :)', Source_h_trans(2,:)', Source_h_trans(3,:)', 'bo');
+    h2 = plot3(Target(:, 1), Target(:, 2), Target(:, 3), 'ro');
+    drawnow;
+
+    % Update Source Point Clouds
+    Source = Source_new;
+
   end
 
 end
